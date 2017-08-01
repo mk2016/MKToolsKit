f8b46909d4766eb7867ac642b099831ed82bb077
MK <xiaomk7758@sina.com>
2017-08-01 16:59:48 +0800

    modify MKAlertView

--- MKToolsKit/MKToolsKit/MKSubassembly/MKAlertView/MKAlertView.h
+++ MKToolsKit/MKToolsKit/MKSubassembly/MKAlertView/MKAlertView.h
@@ -9,15 +9,16 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-@class MKAlertViewConfig;
+@class MKAlertCtrlConfig;
 @interface MKAlertView : NSObject
 
 #pragma mark - ***** system style *****
+
 + (void)alertWithTitle:(NSString *)title
                message:(NSString *)message
-      onViewController:(UIViewController *)vc
-                 block:(void (^)(NSInteger buttonIndex))block
-          buttonTitles:(NSString *)cancelTitle, ... NS_REQUIRES_NIL_TERMINATION;
+           cancelTitle:(NSString *)cancelTitle
+          confirmTitle:(NSString *)confirmTitle
+                 block:(void (^)(NSInteger buttonIndex))block;
 
 + (void)alertWithTitle:(NSString *)title
                message:(NSString *)message
@@ -28,47 +29,30 @@
 
 + (void)alertWithTitle:(NSString *)title
                message:(NSString *)message
-          buttonTitles:(NSArray *)buttonTitles
-      onViewController:(UIViewController *)vc
-                 block:(void (^)(NSInteger buttonIndex))block;
-
-
-
-
-#pragma mark - ***** custom style *****
-+ (void)alertWithTitle:(NSString *)title
-               message:(NSString *)message
-                config:(MKAlertViewConfig *)config
-      onViewController:(UIViewController *)vc
-                 block:(void (^)(NSInteger buttonIndex))block
-          buttonTitles:(NSString *)cancelTitle, ... NS_REQUIRES_NIL_TERMINATION;
-
-+ (void)alertWithTitle:(NSString *)title
-               message:(NSString *)message
            cancelTitle:(NSString *)cancelTitle
           confirmTitle:(NSString *)confirmTitle
-                config:(MKAlertViewConfig *)config
+      onViewController:(UIViewController *)vc
+                config:(MKAlertCtrlConfig *)config
+                 block:(void (^)(NSInteger buttonIndex))block;
+
+
++ (void)alertWithTitle:(NSString *)title
+               message:(NSString *)message
+          buttonTitles:(NSArray *)buttonTitles
+                 block:(void (^)(NSInteger buttonIndex))block;
+
++ (void)alertWithTitle:(NSString *)title
+               message:(NSString *)message
+          buttonTitles:(NSArray *)buttonTitles
       onViewController:(UIViewController *)vc
                  block:(void (^)(NSInteger buttonIndex))block;
 
 + (void)alertWithTitle:(NSString *)title
                message:(NSString *)message
           buttonTitles:(NSArray *)buttonTitles
-                config:(MKAlertViewConfig *)config
       onViewController:(UIViewController *)vc
+                config:(MKAlertCtrlConfig *)config
                  block:(void (^)(NSInteger buttonIndex))block;
+
 @end
 
-
-@interface MKAlertViewConfig : NSObject
-@property (nonatomic, strong) UIFont *titleFont;
-@property (nonatomic, strong) UIColor *titleColor;
-@property (nonatomic, assign) NSTextAlignment titleAlignment;
-@property (nonatomic, strong) UIFont *messageFont;
-@property (nonatomic, strong) UIColor *messageColor;
-@property (nonatomic, assign) NSTextAlignment messageAlignment;
-@property (nonatomic, strong) UIColor *actionColor;
-@property (nonatomic, assign) NSInteger cancelIndex;
-@property (nonatomic, assign) NSInteger destructiveIndex;
-@property (nonatomic, strong) UIColor *destructivelColor;
-@end
